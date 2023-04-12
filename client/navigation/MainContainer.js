8a18521e238d90ff9583cf7b6578b0e428ec4f8a
Vander <ch.gantulga54@gmail.com>
2023-04-13 00:02:20 +0800

    bottom bar oorchlow

--- client/navigation/MainContainer.js
+++ client/navigation/MainContainer.js
@@ -3,58 +3,63 @@ import * as React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import { AntDesign } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
 // Screens
 import Home from "./screens/Home";
 import Workout from "./screens/Workout";
 import Activity from "./screens/Activity";
 import Profile from "./screens/Profile";
 
-//Screen names
-const homename = 'Home';
-const workoutname = 'Workout';
-const profilename = 'Profile';
-const activityname = 'Activity';
-
 const Tab = createBottomTabNavigator();
 
 export default function MainContainer() {
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName={homename}
-        screenOptions={({ route }) => ({
-          tabBarShowLabel: false,
-          tabBarActiveTintColor: '#6A36FF',
-          tabBarInactiveTintColor: '#CBB9FF',
-          tabBarStyle: { padding: 5, height: 50 },
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-            let rn = route.name;
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
 
-            if (rn === homename) {
-              iconName = focused ? 'home' : 'home-outline';
+          if (route.name === 'Home') {
+            iconName = 'home';
+          } else if (route.name === 'Workout') {
+            iconName = 'dumbbell';
+          } else if (route.name === 'Profile') {
+            iconName = 'user';
+          } else if (route.name === 'Activity') {
+            iconName = 'chart-bar';
+          }
 
-            } else if (rn === workoutname) {
-              iconName = focused ? 'compass' : 'compass-outline';
-
-            } else if (rn === activityname) {
-              iconName = focused ? 'person' : 'person-outline';
-            }
-            else if (rn === profilename) {
-              iconName = focused ? 'person' : 'person-outline';
-            }
-
-            // You can return any component that you like here!
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-        })}
-        >
-      <Tab.Screen name={homename} component={Home} />
-      <Tab.Screen name={workoutname} component={Workout} />
-      <Tab.Screen name={activityname} component={Activity} />
-      <Tab.Screen name={profilename} component={Profile} />
-
-      </Tab.Navigator>
+          return <FontAwesome5 name={iconName} size={size} color={color} />;
+        },
+        tabBarStyle: {
+          borderTopWidth: 0,
+          elevation: 0,
+          shadowOpacity: 0,
+          borderBottomWidth: 1,
+          borderColor: 'blue',
+        },
+        tabBarItemStyle: {
+          justifyContent: 'center',
+          alignItems: 'center',
+        },
+        tabBarActiveTintColor: '#6A36FF',
+        tabBarInactiveTintColor: '#CBB9FF',
+        tabBarLabelStyle: {
+          display: 'none',
+        },
+        tabBarIndicatorStyle: {
+          backgroundColor: 'red', // Change this value to adjust the color of the active tab indicator
+          height: 5, // Change this value to adjust the thickness of the active tab indicator
+        },
+      })}
+    >
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Workout" component={Workout} />
+      <Tab.Screen name="Activity" component={Activity} />
+      <Tab.Screen name="Profile" component={Profile} />
+    </Tab.Navigator>
     </NavigationContainer>
   )
 }
