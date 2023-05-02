910981dd1e63d0913e51e6a7143316983cadf972
Vander <ch.gantulga54@gmail.com>
2023-05-02 17:36:50 +0800

    ehlel3

--- client/App.js
+++ client/App.js
@@ -8,12 +8,14 @@ import Costumize from "./views/workout_nav/Costumize";
 import UserScreen from "./navigation/Workout_navs/UserScreen";
 import FreTseej from './views/workout_nav/IsPremuim/FreTseej';
 import PreTseej from './views/workout_nav/IsPremuim/PreTseej';
+import Log from './views/Log';
 const Stack = createNativeStackNavigator()
 function App(){
   const isPremuimUser = false
   return(
     <NavigationContainer>
       <Stack.Navigator>
+      <Stack.Screen name="Log" component={Log} options={{headerShown: false}} />
         <Stack.Screen name="Login_nav" component={Login_nav} options={{headerShown: false}} />
         <Stack.Screen name="Exercises" component={Exercises} options={{headerShown: false}} />
         <Stack.Screen name='Costumize' component={Costumize} options={{headerShown: false}}/>
