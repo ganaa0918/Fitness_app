f9c11fdad77145758c64b65d690bdf0ba6d9f280
tuguldur bayrmagnai <tuguldur.ba@novelsoft.mn>
2023-05-04 11:00:33 +0800

    toogoogin hairiin zahia

---
+++ server/MongodbconnUrl.js
@@ -0,0 +1,3 @@
+module.exports = { 
+    MongoUrl : ' mogoUrl:"put your mongo url here",'
+}
--- server/index.js
+++ server/index.js
@@ -3,7 +3,9 @@ const bodyParser = require('body-parser')
 
 const app = express()
 const PORT = 3000
-
+const {mongoUrl} = require('./keys')
+const WorkoutRoute = require("")
+app.use(WorkoutRoute)
 app.use(bodyParser.json())
 app.get('/', (req, res) =>  { 
     res.send('hello');
