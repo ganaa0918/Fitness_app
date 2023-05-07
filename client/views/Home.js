bc9277a87e1b97c9e46114defef7dd8f77590e57
Vander <ch.gantulga54@gmail.com>
2023-05-07 23:04:26 +0800

    home bas Profile hiisen shvv jvlh2

--- client/views/Home.js
+++ client/views/Home.js
@@ -1,11 +1,252 @@
-import * as React from "react";
-import {View, Text} from "react-native"
-export default function Home({navigation}) {
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet, TouchableOpacity, Image, ScrollView } from "react-native";
+import { ProgressBar } from "react-native-paper";
+import { LinearGradient } from 'expo-linear-gradient';
+import Swiper from "react-native-swiper";
+import img1 from '../assets/daily.png'
+import img2 from '../assets/daily1.png'
+import img3 from '../assets/daily2.png'
+import img4 from '../assets/daily3.png'
+import img5 from '../assets/daily4.png'
+import img6 from '../assets/daily5.png'
+
+import { FontAwesome5 } from '@expo/vector-icons';
+
+export default function Home({ navigation }) {
+  const [showSearch, setShowSearch] = useState(false);
+  const [searchText, setSearchText] = useState("");
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity
+          style={{ marginRight: 15 }}
+          onPress={() => {
+            // Perform search action
+          }}
+        >
+          <FontAwesome5 name="search" size={24} color="black" />
+        </TouchableOpacity>
+      ),
+    });
+  }, []);
+  const [progress, setProgress] = useState(0.5);
+
+  const images = [
+    { id: "1", source: require("../assets/Rectangle20.png"), text: "Deadlift буюу үндсэн таталтыг зөв \n хийх 4 арга техник", screen:"Home" },
+    { id: "2", source: require("../assets/Rectangle21.png"), text: "Хүнсний ногоог хэрэглэх хамгийн \n эрүүл арга юу вэ?", screen:"Home" },
+    { id: "3", source: require("../assets/Rectangle22.png"), text: "Сайн нүүрс ус гэж юу вэ", screen:"Home" },
+    // Add more images here...
+  ];
+
+  const renderCarouselItem = (item) => (
+    <TouchableOpacity
+      style={styles.carouselItem}
+      onPress={() => navigation.navigate(item.screen)}
+    >
+
+      <Image source={item.source} style={styles.image} />
+      <Text style={styles.textnews}>{item.text}</Text>
+    </TouchableOpacity>
+  );
+
   return (
-    <View classname="flex-1, justify-center, align-items-center">
-        <Text classname="text-blue-600" onPress={()=>{
-            alert('This is Home ')
-        }}>Home screen</Text>
+    <ScrollView contentContainerStyle={styles.scrollContainer}>
+    <View style={styles.container}>
+      <LinearGradient
+        colors={['#C58BF2', '#6358E1']}
+        style={styles.gradient}
+        start={{ x: 1, y: 0.8 }}
+        end={{ x: -0.2, y: 0.7 }}
+        locations={[0, 1]}
+      >
+        <View style={styles.text1}>
+          <Text style={{ color: 'white' }}>Долоо хонгийн идэвх</Text>
+          <Text style={{ color: 'white' }}>{`${progress * 4}/4`}</Text>
+        </View>
+        <ProgressBar progress={progress} color="#4B40CE" style={{ width: "90%", height: 15, borderRadius: 10, left: 20 }} />
+      </LinearGradient>
+      <View style={styles.readall}>
+        <Text style={{ marginRight: 80, fontSize: 20 }}>Зөвлөгөө Зөвлөмж</Text>
+        <TouchableOpacity style={{ paddingTop: 5 }}>
+          <Text style={{ color: 'gray' }}>Бүгдийг үзэх</Text>
+        </TouchableOpacity>
+      </View>
+      <Swiper
+        style={styles.carousel}
+        loop={false}
+        autoplay={false}
+        showsPagination={false}
+        showsButtons={false}
+      >
+        {images.map((item) => (
+          <View key={item.id}>{renderCarouselItem(item)}</View>
+        ))}
+      </Swiper>
+      <View style={styles.dasgaluudContainer}>
+      <Text style={styles.dasgaluudText}>Энгийн дасгалууд</Text>
+      <Image source={img1} />
+      <View style={styles.dailycontainer}>
+        <View style={styles.Alignbetween2}>
+          <Image source={img2} />
+          <View style={{ flexDirection: 'column', marginLeft:20 }}>
+            <Text style={styles.text4}>Abs Эхлэгч</Text>
+            <Text style={styles.text3}>18 mins</Text>
+          </View>
+          <View style={{ flex: 1, alignItems: 'flex-end' }}>
+            <TouchableOpacity
+            style={styles.roundButton2}>
+            <FontAwesome5 name='play' size={10} color='#FFFFFF' style={{paddingTop:8}} solid/>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View style={styles.Alignbetween2}>
+          <Image source={img3} />
+          <View style={{ flexDirection: 'column', marginLeft:20 }}>
+            <Text style={styles.text4}>Цээж Эхлэгч</Text>
+            <Text style={styles.text3}>12 mins</Text>
+          </View>
+          <View style={{ flex: 1, alignItems: 'flex-end' }}>
+            <TouchableOpacity
+            style={styles.roundButton2}>
+            <FontAwesome5 name='play' size={10} color='#FFFFFF' style={{paddingTop:8}} solid/>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View style={styles.Alignbetween2}>
+          <Image source={img4} />
+          <View style={{ flexDirection: 'column', marginLeft:20 }}>
+            <Text style={styles.text4}>Мөр Эхлэгч</Text>
+            <Text style={styles.text3}>15 mins</Text>
+          </View>
+          <View style={{ flex: 1, alignItems: 'flex-end' }}>
+            <TouchableOpacity
+            style={styles.roundButton2}>
+            <FontAwesome5 name='play' size={10} color='#FFFFFF' style={{paddingTop:8}} solid/>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View style={styles.Alignbetween2}>
+          <Image source={img5} />
+          <View style={{ flexDirection: 'column', marginLeft:20 }}>
+            <Text style={styles.text4}>Хөл Эхлэгч </Text>
+            <Text style={styles.text3}>15 mins</Text>
+          </View>
+          <View style={{ flex: 1, alignItems: 'flex-end' }}>
+            <TouchableOpacity
+            style={styles.roundButton2}>
+            <FontAwesome5 name='play' size={10} color='#FFFFFF' style={{paddingTop:8}} solid/>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View style={styles.Alignbetween2}>
+          <Image source={img6} />
+          <View style={{ flexDirection: 'column', marginLeft:20 }}>
+            <Text style={styles.text4}>Нуруу Эхлэгч</Text>
+            <Text style={styles.text3}>15 mins</Text>
+          </View>
+          <View style={{ flex: 1, alignItems: 'flex-end' }}>
+            <TouchableOpacity
+            style={styles.roundButton2}>
+            <FontAwesome5 name='play' size={10} color='#FFFFFF' style={{paddingTop:8}} solid/>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
     </View>
-    )
+    </View>
+    </ScrollView>
+  );
 }
+
+const styles = StyleSheet.create({
+  scrollContainer: {
+    flexGrow: 1,
+    paddingBottom: 10,
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: "center",
+  },
+  Alignbetween2: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    width: '100%',
+    paddingVertical: 20,
+  },
+  gradient: {
+    width: 350,
+        height:75,
+        borderRadius:10,
+    },
+    readall: {
+        justifyContent:'space-between',
+        flexDirection: 'row',
+        paddingHorizontal: 0,
+        paddingVertical: 10,
+    },
+      carouselItem: {
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 10,
+      },
+      image: {
+        width: 350,
+        height: 400,
+        borderRadius:20,
+        resizeMode:'stretch'
+      },
+      textnews:{
+        position:'absolute',
+        left:0,
+        right:10,
+        bottom:200,
+        marginTop: 10,
+        fontSize: 16,
+        fontWeight: "bold",
+        textAlign: "center",
+        color: 'white',
+        fontSize:20,
+      },
+      text2:{
+        right:100,
+        position: "relative",
+        bottom:100,
+        fontSize:20,
+
+      },
+      text1:{
+        flexDirection: "row",
+        padding:10,
+        justifyContent:'space-between',
+        paddingHorizontal:20
+
+      },
+      dasgaluudContainer: {
+        marginBottom: 10,
+        position: "relative",
+        bottom:350,
+      },
+      dasgaluudText: {
+        fontSize: 20,
+        textAlign:'left',
+        paddingBottom:10
+      },
+      dailycontainer: {
+        width:360,
+        height:500,
+        backgroundColor: '#FFFFFF',
+      },
+      roundButton2: {
+        position: 'relative',
+        width: 30,
+        height: 30,
+        borderColor: '#000',
+        borderRadius: 20,
+        borderWidth:1,
+        alignItems: 'center',
+        backgroundColor:'#000',
+        alignContent:'center',
+      },
+})
--- client/views/Profile.js
+++ client/views/Profile.js
@@ -1,9 +1,222 @@
-import * as React from "react";
-import {View, Text} from "react-native"
+import React, {useState} from "react";
+import {View, Text, StyleSheet,Image, TouchableOpacity,ScrollView} from "react-native"
+import { FontAwesome5 } from '@expo/vector-icons';
+import Toggle from 'react-native-toggle-input'
+import img from '../assets/profile.png'
+
 export default function Profile({navigation}) {
+  const [toggle, setToggle] = useState(false);
+  const toggleColor = toggle ? '#4C956C' : '#DB4437';
   return (
-    <View classname="flex-1, justify-center, align-items-center">
-        <Text classname="text-blue-600" onPress={()=> navigation.navigate('Home')}>Activity screen</Text>
+    <ScrollView contentContainerStyle={styles.scrollContainer}>
+
+    <View style={styles.container}>
+    <View style={styles.Alignbetween2}>
+        <View style={{ flexDirection: 'column', marginLeft: 20, marginTop:10 }}>
+          <Text style={styles.text4}>Vander</Text>
+          <Text style={styles.text3}>Lose a Fat Program</Text>
+        </View>
+        <View style={styles.back}>
+          <Image source={img} style={styles.image} />
+        </View>
+      </View>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.textvip}>Pro хэрэглэгч болох</Text>
+      </TouchableOpacity>
+      <Text style={styles.account}>Аккоунт</Text>
+      <View style={styles.acccontainer}>
+        <View style={styles.Alignbetween3}>
+            <FontAwesome5 name='user' size={20} color='#92A5FD' style={{paddingTop:5}} solid/>
+              <Text style={styles.text5}>Хувийн мэдээлэл</Text>
+            <View style={{ flex: 1, alignItems: 'flex-end' }}>
+              <TouchableOpacity style={styles.ooo}>
+              <FontAwesome5 name='chevron-right' size={20} color='gray' solid/>
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View style={styles.Alignbetween3}>
+            <FontAwesome5 name='file-invoice' size={20} color='#92A5FD' style={{paddingTop:5}} solid/>
+              <Text style={styles.text5}>Хэрэглэгчийн календарь</Text>
+            <View style={{ flex: 1, alignItems: 'flex-end' }}>
+              <TouchableOpacity style={styles.ooo}>
+              <FontAwesome5 name='chevron-right' size={20} color='gray' solid/>
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View style={styles.Alignbetween3}>
+            <FontAwesome5 name='chart-pie' size={20} color='#92A5FD' style={{paddingTop:5}} solid/>
+              <Text style={styles.text5}>Дасгалын түүх</Text>
+            <View style={{ flex: 1, alignItems: 'flex-end' }}>
+              <TouchableOpacity style={styles.ooo}>
+              <FontAwesome5 name='chevron-right' size={20} color='gray' solid/>
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View style={styles.Alignbetween3}>
+            <FontAwesome5 name='chart-bar' size={20} color='#92A5FD' style={{paddingTop:5}} solid/>
+              <Text style={styles.text5}>Миний дасгалын төлөвлгөө</Text>
+            <View style={{ flex: 1, alignItems: 'flex-end' }}>
+              <TouchableOpacity style={styles.ooo}>
+              <FontAwesome5 name='chevron-right' size={20} color='gray' solid/>
+              </TouchableOpacity>
+            </View>
+          </View>
+      </View>
+      <View style={styles.Msg}>
+        <Text style={styles.text6}>Мэдэгдэл</Text>
+        <View style={styles.Alignbetween4}>
+            <FontAwesome5 name='bell' size={20} color='#92A5FD' style={{paddingTop:0}} solid/>
+              <Text style={styles.text5} >Мэдэгдэл ирэх</Text>
+            <View style={{ flex: 1, alignItems: 'flex-end' }}>
+            <Toggle toggle={toggle} size={20} color={"#92A5FD"} filled={true} circleColor={"white"} setToggle={setToggle} />
+            </View>
+          </View>
+      </View>
+      <View style={styles.Other}>
+        <Text style={styles.text6}>Бусад</Text>
+        <View style={styles.Alignbetween4}>
+            <FontAwesome5 name='envelope' size={20} color='#92A5FD' style={{paddingTop:0}} solid/>
+              <Text style={styles.text5} >Бидэнтэй холбогдох</Text>
+            <View style={{ flex: 1, alignItems: 'flex-end' }}>
+            <TouchableOpacity style={styles.ooo}>
+              <FontAwesome5 name='chevron-right' size={20} color='gray' solid/>
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View style={styles.Alignbetween4}>
+            <FontAwesome5 name='user-secret' size={20} color='#92A5FD' style={{paddingTop:0}} solid/>
+              <Text style={styles.text5} >Нууцлал</Text>
+            <View style={{ flex: 1, alignItems: 'flex-end' }}>
+            <TouchableOpacity style={styles.ooo}>
+              <FontAwesome5 name='chevron-right' size={20} color='gray' solid/>
+              </TouchableOpacity>
+            </View>
+          </View>
+      </View>
     </View>
+    </ScrollView>
     )
 }
+const styles = StyleSheet.create({
+  scrollContainer: {
+    flexGrow: 1,
+    paddingBottom: 100,
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: "center",
+    backgroundColor:"white"
+  },
+  Alignbetween2: {
+    flexDirection: 'row',
+    paddingHorizontal: 20,
+    width: '100%',
+    paddingVertical: 20,
+    justifyContent:'space-between'
+  },
+  Alignbetween2: {
+    flexDirection: 'row',
+    paddingHorizontal: 20,
+    width: '100%',
+    paddingVertical: 20,
+  },
+  image: {
+    width:75,
+    height:75
+  },
+  text4: {
+    fontSize: 20,
+  },
+  text3: {
+    fontSize: 16,
+    color: "gray",
+  },
+  back: {
+    backgroundColor:"#B9B5EC",
+    borderRadius: 60,
+    position:"relative",
+    width:75,
+    height:75,
+    left:100
+  },
+  button: {
+    width:250,
+    height:50,
+    backgroundColor:"#FFF500",
+    borderRadius: 20,
+    alignItems: "center",
+    position: "relative",
+    paddingTop:20
+  },
+  textvip:{
+    position: "absolute",
+    marginTop:15,
+    color: "#7B6F72"
+  },
+  account:{
+    right:120,
+    fontSize:20,
+    paddingVertical:20
+  },
+  ooo :{
+    right:20
+  },
+  text5:{
+    color: "#7B6F72",
+    paddingHorizontal:20
+  },
+  Alignbetween3: {
+    position:'relative',
+    flexDirection: 'row',
+    paddingHorizontal: 20,
+    width: '100%',
+    paddingVertical: 10,
+  },
+  Alignbetween4: {
+    position:'relative',
+    flexDirection: 'row',
+    paddingHorizontal: 20,
+    width: '100%',
+    paddingVertical: 10,
+  },
+  acccontainer: {
+    position:'relative',
+    backgroundColor:"#FFF",
+    width:350,
+    height:200,
+    shadowColor:"#000",
+    shadowOffset:{ width:5, height:3},
+    ShadowOpacity: 0.5,
+    shadowRadius:20,
+    elevation: 5,
+  },
+  Msg:{
+    position:"relative",
+    backgroundColor:"#FFFFFF",
+    top:20,
+    width: 350,
+    height: 75,
+    shadowColor:"#000",
+    shadowOffset:{ width:5, height:3},
+    ShadowOpacity: 0.5,
+    shadowRadius:20,
+    elevation: 5
+  },
+  text6: {
+    left:20,
+    fontSize:20
+  },
+  Other:{
+    position:"relative",
+    backgroundColor:"#FFFFFF",
+    top: 60,
+    width: 350,
+    height: 120,
+    shadowColor:"#000",
+    shadowOffset:{ width:5, height:3},
+    ShadowOpacity: 0.5,
+    shadowRadius:20,
+    elevation: 5
+  },
+})
