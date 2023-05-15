22429270504772f6b259a7e0b16e0bba0f013bbe
tuguldur bayrmagnai <tuguldur.ba@novelsoft.mn>
2023-05-15 22:28:44 +0800

    done ganaa

--- client/views/activity_nav/Data.js
+++ client/views/activity_nav/Data.js
@@ -1,13 +1,42 @@
 import * as React from "react";
 import {View, Text} from "react-native"
+import {
+  StyleSheet,
+  Dimensions,
+  ScrollView,
+} from 'react-native';
+
+//import React Native chart Kit for different kind of Chart
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart,
+} from 'react-native-chart-kit';
 export default function Activity({navigation}) {
   return (
     <View classname="flex-1, justify-center, align-items-center">
     
-      <Text>Progress Chart</Text>
-      {/* Todo Progress bar zas */}
-      {/* <ProgressChart
-        data={[0.4, 0.6, 0.8]}
+ 
+      <Text classname="text-blue-600" style={{
+          marginTop: 15,
+          margin: 25,
+          marginVertical: 8,
+          borderRadius: 16,
+          fontSize: 20
+        }}   >Activity screen</Text>
+        <LineChart
+        data={{
+          labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat' , 'Sun'],
+          datasets: [
+            {
+              data: [20, 45, 28, 80, 99, 43, 50],
+              strokeWidth: 2,
+            },
+          ],
+        }}
         width={Dimensions.get('window').width - 16}
         height={220}
         chartConfig={{
@@ -21,10 +50,46 @@ export default function Activity({navigation}) {
           },
         }}
         style={{
+          marginTop: 5,
           marginVertical: 8,
           borderRadius: 16,
         }}
-      /> */}
+      />
+         <Text classname="text-blue-600" style={{
+          marginTop: 15,
+          margin: 25,
+          marginVertical: 8,
+          borderRadius: 16,
+          fontSize: 20
+        }}   >Activity screen</Text>
+        <LineChart
+        data={{
+          labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat' , 'Sun'],
+          datasets: [
+            {
+              data: [20, 45, 28, 80, 99, 43, 50],
+              strokeWidth: 2,
+            },
+          ],
+        }}
+        width={Dimensions.get('window').width - 16}
+        height={220}
+        chartConfig={{
+          backgroundColor: '#1cc910',
+          backgroundGradientFrom: '#eff3ff',
+          backgroundGradientTo: '#efefef',
+          decimalPlaces: 2,
+          color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+          style: {
+            borderRadius: 16,
+          },
+        }}
+        style={{
+          marginTop: 5,
+          marginVertical: 8,
+          borderRadius: 16,
+        }}
+      />
     </View>
     )
 }
--- client/views/activity_nav/Days.js
+++ client/views/activity_nav/Days.js
@@ -1,9 +1,58 @@
 import * as React from "react";
 import {View, Text} from "react-native"
+import {
+  StyleSheet,
+  Dimensions,
+  ScrollView,
+} from 'react-native';
+
+//import React Native chart Kit for different kind of Chart
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart,
+} from 'react-native-chart-kit';
 export default function Activity({navigation}) {
   return (
     <View classname="flex-1, justify-center, align-items-center">
-        <Text classname="text-blue-600" onPress={()=> navigation.navigate('Home')}>Activity screen</Text>
+           <Text classname="text-blue-600" style={{
+          marginTop: 15,
+          margin: 25,
+          marginVertical: 8,
+          borderRadius: 16,
+          fontSize: 20
+        }}   >Activity screen</Text>
+            <BarChart
+        data={{
+          labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+          datasets: [
+            {
+              data: [20, 45, 28, 80, 99, 43],
+            },
+          ],
+        }}
+        width={Dimensions.get('window').width - 16}
+        height={220}
+        yAxisLabel={'Rs'}
+        chartConfig={{
+          backgroundColor: '#1cc910',
+          backgroundGradientFrom: '#eff3ff',
+          backgroundGradientTo: '#efefef',
+          decimalPlaces: 2,
+          color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+          style: {
+            borderRadius: 16,
+          },
+        }}
+        style={{
+          marginVertical: 8,
+          borderRadius: 16,
+        }}
+      />  
+     
     </View>
     )
 }
